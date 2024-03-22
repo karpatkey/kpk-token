be362f13f2ddaa984034eff4d87178f0f35fe3f2
sgzerbo <santiago@karpatkey.com>
2024-03-22 21:18:29 +0000

    feat: transferAllowance

--- solidity/contracts/ZTestToken.sol
+++ solidity/contracts/ZTestToken.sol
@@ -18,9 +18,22 @@ contract ZTestToken is ERC20, Pausable, Ownable {
   error TransferToTokenContract();
 
   /**
-   * @dev Token transfers can only be initialized by contract owner when paused.
+   * @dev Indicates a failure with the caller's `transferAllowance`. Used in transfers.
+   * @param owner Address that may be allowed to transfer tokens.
+   * @param transferAllowance Amount of tokens a the owner is allowed to transfer.
+   * @param needed Minimum amount required to perform a transfer.
    */
-  error TransfersUnathorizedWhenPaused();
+  error InsufficientTransferAllowance(address owner, uint256 transferAllowance, uint256 needed);
+
+  error InvalidTransferApproval(address owner);
+
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event TransferApproval(address indexed owner, uint256 value);
+
+  mapping(address account => uint256) private _transferAllowances;
 
   function pause() public onlyOwner {
     _pause();
@@ -28,16 +41,6 @@ contract ZTestToken is ERC20, Pausable, Ownable {
 
   function unpause() public onlyOwner {
     _unpause();
-  }
-
-  function _update(address from, address to, uint256 value) internal override(ERC20) {
-    if (to == address(this)) {
-      revert TransferToTokenContract();
-    }
-    if (paused() && owner() != _msgSender()) {
-      revert TransfersUnathorizedWhenPaused();
-    }
-    super._update(from, to, value);
   }
 
   /**
@@ -53,6 +56,19 @@ contract ZTestToken is ERC20, Pausable, Ownable {
     return true;
   }
 
+  function transferAllowance(address owner) public view virtual returns (uint256) {
+    return _transferAllowances[owner];
+  }
+
+  /**
+   * @dev Destroys a `value` amount of tokens from the token holder 'owner'.
+   *
+   * See {ERC20-_burn}.
+   */
+  function approveTransfer(address owner, uint256 value) public virtual onlyOwner {
+    _approveTransfer(owner, value);
+  }
+
   function mint(address to, uint256 amount) public onlyOwner {
     _mint(to, amount);
   }
@@ -64,5 +80,44 @@ contract ZTestToken is ERC20, Pausable, Ownable {
    */
   function burn(address owner, uint256 value) public virtual onlyOwner {
     _burn(owner, value);
+  }
+
+  function _approveTransfer(address owner, uint256 value) internal virtual {
+    if (owner == address(0)) {
+      revert InvalidTransferApproval(address(0));
+    }
+    _transferAllowances[owner] = value;
+    emit TransferApproval(owner, value);
+  }
+
+  function _update(address from, address to, uint256 value) internal override(ERC20) {
+    if (to == address(this)) {
+      revert TransferToTokenContract();
+    }
+    uint256 allowance = transferAllowance(from);
+    if (paused() && owner() != _msgSender() && allowance < value) {
+      revert InsufficientTransferAllowance(from, allowance, value);
+    }
+    super._update(from, to, value);
+  }
+
+  /**
+   * @dev Updates `owner` s transfer allowance based on spent `value`.
+   *
+   * Does not update the transferAllowance value in case of infinite allowance.
+   * Revert if not enough transfer allowance is available.
+   *
+   * Does not emit an {Approval} event.
+   */
+  function _spendTransferAllowance(address owner, uint256 value) internal virtual {
+    uint256 currentTransferAllowance = transferAllowance(owner);
+    if (currentTransferAllowance != type(uint256).max) {
+      if (currentTransferAllowance < value) {
+        revert InsufficientTransferAllowance(owner, currentTransferAllowance, value);
+      }
+      unchecked {
+        _approve(owner, owner, currentTransferAllowance - value, false);
+      }
+    }
   }
 }
