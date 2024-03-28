5068025edefb5953e2ae836251f5f601ae7a5352
sgzerbo <santiago@karpatkey.com>
2024-03-28 20:51:47 +0000

    feat: rescue token test

--- solidity/test/unit/ZTestToken.t.sol
+++ solidity/test/unit/ZTestToken.t.sol
@@ -32,7 +32,7 @@ contract TestConstructor is Base {
 }
 
 contract TestPause is Base {
-  function test_UnpauseExpectedRevert() public {
+  function test_UnpauseExpectedRevertOwner() public {
     address _randomAddress = makeAddr('randomAddress');
     vm.startPrank(_randomAddress);
     vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, _randomAddress));
@@ -45,7 +45,7 @@ contract TestPause is Base {
     assertEq(_ztoken.paused(), false);
   }
 
-  function test_PauseExpectedRevert() public {
+  function test_PauseExpectedRevertOwner() public {
     vm.prank(_owner);
     _ztoken.unpause();
     address _randomAddress = makeAddr('randomAddress');
@@ -65,7 +65,7 @@ contract TestTransferOwnership is Base {
     assertEq(_ztoken.transferAllowance(_owner), 0);
   }
 
-  function test_transferOwnershipExpectedRevert() public {
+  function test_transferOwnershipExpectedRevertOwner() public {
     address _randomAddress = makeAddr('randomAddress');
     address _newOwner = makeAddr('newOwner');
     vm.startPrank(_randomAddress);
@@ -80,11 +80,13 @@ contract TestTransferAllowance is Base {
     uint256 amount = 100;
     vm.startPrank(_owner);
     assertEq(_ztoken.transferAllowance(_holder), 0);
+    //vm.expectEmit(address(_ztoken));
+    //emit IZTT.TransferApproval(_holder, amount);
     _ztoken.approveTransfer(_holder, amount);
     assertEq(_ztoken.transferAllowance(_holder), amount);
   }
 
-  function test_transferAllowanceExpectedRevert() public {
+  function test_transferAllowanceExpectedRevertOwner() public {
     address _randomAddress = makeAddr('randomAddress');
     uint256 amount = 100;
     vm.startPrank(_randomAddress);
@@ -153,11 +155,49 @@ contract TestTransfers is BaseTransfer {
     assertEq(_ztoken.balanceOf(_holder), 1);
   }
 
-  function test_transferByOwnerExpectedRevert() public {
+  function test_transferByOwnerExpectedRevertOwner() public {
     address _randomAddress = makeAddr('randomAddress');
     address _recipient = makeAddr('recipient');
     vm.startPrank(_randomAddress);
     vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, _randomAddress));
     _ztoken.transferByOwner(_holder, _recipient, amount - 1);
+  }
+}
+
+abstract contract BaseRescueToken is Base {
+  uint256 internal constant _FORK_BLOCK = 19_534_932;
+  address internal _daiWhale = 0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016;
+  IERC20 internal _dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+  uint256 internal amount = 100;
+
+  function setUp() public virtual override(Base) {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), _FORK_BLOCK);
+    super.setUp();
+    vm.prank(_daiWhale);
+    _dai.transfer(address(_ztoken), amount);
+  }
+}
+
+contract TestRescueToken is BaseRescueToken {
+  function test_rescueToken() public {
+    address _beneficiary = makeAddr('beneficiary');
+    vm.startPrank(_owner);
+    _ztoken.rescueToken(_dai, _beneficiary, amount - 1);
+    assertEq(_dai.balanceOf(_beneficiary), amount - 1);
+    assertEq(_dai.balanceOf(address(_ztoken)), 1);
+  }
+
+  function test_rescueTokenExpectedRevert() public {
+    address _beneficiary = makeAddr('beneficiary');
+    vm.startPrank(_owner);
+    vm.expectRevert(abi.encodeWithSelector(IZTT.NotEnoughBalanceToRescue.selector, _dai, amount + 1));
+    _ztoken.rescueToken(_dai, _beneficiary, amount + 1);
+  }
+
+  function test_rescueTokenExpectedRevertOwner() public {
+    address _randomAddress = makeAddr('randomAddress');
+    vm.startPrank(_randomAddress);
+    vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, _randomAddress));
+    _ztoken.rescueToken(_dai, _randomAddress, amount);
   }
 }
