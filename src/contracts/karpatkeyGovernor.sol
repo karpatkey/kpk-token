4307c43e004ac114794686554e7b1864149809cc
sgzerbo <santiago@karpatkey.com>
2024-04-04 16:25:55 +0100

    feat: upgradeable governor

--- src/contracts/karpatkeyGovernor.sol
+++ src/contracts/karpatkeyGovernor.sol
@@ -2,62 +2,87 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import '@openzeppelin/contracts/governance/Governor.sol';
+import {GovernorUpgradeable} from '@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol';
 
-import '@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol';
-import '@openzeppelin/contracts/governance/extensions/GovernorSettings.sol';
+import {GovernorCountingSimpleUpgradeable} from
+  '@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol';
+import {GovernorSettingsUpgradeable} from
+  '@openzeppelin/contracts-upgradeable/governance/extensions/GovernorSettingsUpgradeable.sol';
 
-import '@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol';
-import '@openzeppelin/contracts/governance/extensions/GovernorVotes.sol';
-import '@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol';
+import {
+  GovernorTimelockControlUpgradeable,
+  TimelockControllerUpgradeable
+} from '@openzeppelin/contracts-upgradeable/governance/extensions/GovernorTimelockControlUpgradeable.sol';
+import {GovernorVotesQuorumFractionUpgradeable} from
+  '@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol';
+import {
+  GovernorVotesUpgradeable,
+  IVotes
+} from '@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesUpgradeable.sol';
 
-contract karpatkeyGovernor is
-  Governor,
-  GovernorSettings,
-  GovernorCountingSimple,
-  GovernorVotes,
-  GovernorVotesQuorumFraction,
-  GovernorTimelockControl
+import {Initializable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+
+contract MyGovernor is
+  Initializable,
+  GovernorUpgradeable,
+  GovernorSettingsUpgradeable,
+  GovernorCountingSimpleUpgradeable,
+  GovernorVotesUpgradeable,
+  GovernorVotesQuorumFractionUpgradeable,
+  GovernorTimelockControlUpgradeable
 {
-  constructor(
-    IVotes _token,
-    TimelockController _timelock
-  )
-    Governor('DaoGovernor')
-    GovernorSettings(7200, /* 1 day */ 50_400, /* 1 week */ 0)
-    GovernorVotes(_token)
-    GovernorVotesQuorumFraction(4)
-    GovernorTimelockControl(_timelock)
-  {}
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(IVotes _token, TimelockControllerUpgradeable _timelock) public initializer {
+    __Governor_init('MyGovernor');
+    __GovernorSettings_init(7200, /* 1 day */ 50_400, /* 1 week */ 0);
+    __GovernorCountingSimple_init();
+    __GovernorVotes_init(_token);
+    __GovernorVotesQuorumFraction_init(4);
+    __GovernorTimelockControl_init(_timelock);
+  }
 
   // The following functions are overrides required by Solidity.
 
-  function votingDelay() public view override(Governor, GovernorSettings) returns (uint256) {
+  function votingDelay() public view override(GovernorUpgradeable, GovernorSettingsUpgradeable) returns (uint256) {
     return super.votingDelay();
   }
 
-  function votingPeriod() public view override(Governor, GovernorSettings) returns (uint256) {
+  function votingPeriod() public view override(GovernorUpgradeable, GovernorSettingsUpgradeable) returns (uint256) {
     return super.votingPeriod();
   }
 
-  function quorum(uint256 blockNumber) public view override(Governor, GovernorVotesQuorumFraction) returns (uint256) {
+  function quorum(uint256 blockNumber)
+    public
+    view
+    override(GovernorUpgradeable, GovernorVotesQuorumFractionUpgradeable)
+    returns (uint256)
+  {
     return super.quorum(blockNumber);
   }
 
-  function state(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (ProposalState) {
+  function state(uint256 proposalId)
+    public
+    view
+    override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+    returns (ProposalState)
+  {
     return super.state(proposalId);
   }
 
   function proposalNeedsQueuing(uint256 proposalId)
     public
     view
-    override(Governor, GovernorTimelockControl)
+    override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
     returns (bool)
   {
     return super.proposalNeedsQueuing(proposalId);
   }
 
-  function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
+  function proposalThreshold() public view override(GovernorUpgradeable, GovernorSettingsUpgradeable) returns (uint256) {
     return super.proposalThreshold();
   }
 
@@ -67,7 +92,7 @@ contract karpatkeyGovernor is
     uint256[] memory values,
     bytes[] memory calldatas,
     bytes32 descriptionHash
-  ) internal override(Governor, GovernorTimelockControl) returns (uint48) {
+  ) internal override(GovernorUpgradeable, GovernorTimelockControlUpgradeable) returns (uint48) {
     return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
   }
 
@@ -77,7 +102,7 @@ contract karpatkeyGovernor is
     uint256[] memory values,
     bytes[] memory calldatas,
     bytes32 descriptionHash
-  ) internal override(Governor, GovernorTimelockControl) {
+  ) internal override(GovernorUpgradeable, GovernorTimelockControlUpgradeable) {
     super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
   }
 
@@ -86,11 +111,16 @@ contract karpatkeyGovernor is
     uint256[] memory values,
     bytes[] memory calldatas,
     bytes32 descriptionHash
-  ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
+  ) internal override(GovernorUpgradeable, GovernorTimelockControlUpgradeable) returns (uint256) {
     return super._cancel(targets, values, calldatas, descriptionHash);
   }
 
-  function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
+  function _executor()
+    internal
+    view
+    override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+    returns (address)
+  {
     return super._executor();
   }
 }
