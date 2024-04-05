ebbf5304b12a89628c72b41a30988a08f62cb01d
sgzerbo <santiago@karpatkey.com>
2024-04-05 05:08:52 +0100

    feat: remove unused imports

--- test/integration/integration_test.sol
+++ test/integration/integration_test.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
-
-import {IERC20Errors} from '@openzeppelin/contracts/interfaces/draft-IERC6093.sol';
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
 import {karpatkeyToken} from 'contracts/karpatkeyToken.sol';
 import {Test} from 'forge-std/Test.sol';
 import {Upgrades} from 'openzeppelin-foundry-upgrades/Upgrades.sol';
